5d953e2afc11f7a1db4638639b34f62c1b690a09
MD SAMIUL HASAN <samiularfinmd@gmail.com>
2026-09-06 11:39:25 +0600

    Web Dynamic fix

--- src/lib/googleDrive.js
+++ src/lib/googleDrive.js
@@ -1,11 +1,36 @@
 import { google } from 'googleapis';
 
-const auth = new google.auth.GoogleAuth({
-  credentials: {
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  },
-  scopes: ['https://www.googleapis.com/auth/drive'],
-});
+let driveInstance = null;
 
-export const drive = google.drive({ version: 'v3', auth });
+function initDrive() {
+  if (driveInstance) return driveInstance;
+
+  const clientEmail = process.env.GOOGLE_CLIENT_EMAIL;
+  const privateKey = process.env.GOOGLE_PRIVATE_KEY;
+
+  if (!clientEmail || !privateKey) {
+    throw new Error(
+      'Google Drive credentials missing: GOOGLE_CLIENT_EMAIL / GOOGLE_PRIVATE_KEY not set'
+    );
+  }
+
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: clientEmail,
+      private_key: privateKey.replace(/\\n/g, '\n'),
+    },
+    scopes: ['https://www.googleapis.com/auth/drive'],
+  });
+
+  driveInstance = google.drive({ version: 'v3', auth });
+  return driveInstance;
+}
+
+export const drive = new Proxy(
+  {},
+  {
+    get(_target, prop) {
+      return initDrive()[prop];
+    },
+  }
+);
